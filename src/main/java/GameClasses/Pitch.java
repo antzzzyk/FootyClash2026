0dc8be5fa84372710b4fa024bb2f62cd06ceff44
Ninjenzo <enzo.khatamian@gmail.com>
2026-04-09 10:50:03 -0400

    Pitch stuff

--- src/main/java/GameClasses/Pitch.java
+++ src/main/java/GameClasses/Pitch.java
@@ -1,13 +1,26 @@
 package GameClasses;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Pitch {
-    private static double frictionCoefficient = .3;
+    private static final float FRICTIONCOEFFICIENT = .8f;
+    private Image image;
+    private ImageView bg;
 
-    Pitch() {
-
+    public Pitch() {
+        image = new Image(
+                getClass().getResource("/assets/textures/match.png").toExternalForm());
+        bg = new ImageView(image);
+        bg.setFitWidth(1100);
+        bg.setFitHeight(600);
     }
 
-    public static double getFrictionCoefficient() {
-        return frictionCoefficient;
+    public static float getFrictioncoefficient(){
+        return FRICTIONCOEFFICIENT;
+    }
+
+    public ImageView getBg(){
+        return bg;
     }
 }
